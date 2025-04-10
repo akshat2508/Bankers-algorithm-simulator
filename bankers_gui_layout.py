d7046f28b3b58d4c92e3a97b55208e267b4ed3de
Nandini <nansharma77@gmail.com>
2025-04-10 14:11:01 +0530

    added attribute for wait for graph button and implemented allocation matrix

--- bankers_gui_layout.py
+++ bankers_gui_layout.py
@@ -9,6 +9,7 @@ class BankersGUILayout:
         self.need_outputs = []
         self.available_inputs = []
         self.include_rag_button = True  # Set to True by default
+        self.include_wfg_button = True
         
     def create_layout(self, num_processes, num_resources):
         """Create the GUI layout with improved sizing."""
@@ -21,3 +22,11 @@ class BankersGUILayout:
             [sg.Text('Number of Resources:', font=font), sg.Input(num_resources, key='-NUM_RESOURCES-', size=(5, 1), font=font)],
             [sg.Button('Create Matrices', key='-CREATE-', font=font, size=(18, 1))]
         ]
+
+        # Allocation matrix
+        allocation_layout = [[sg.Text(' ', font=header_font)] + [sg.Text(f'R{j}', size=(4, 1), font=header_font) for j in range(num_resources)]]
+        for i in range(num_processes):
+            row = [sg.Text(f'P{i}', font=font)]
+            for j in range(num_resources):
+                row.append(sg.Input('0', key=self.allocation_inputs[i][j], size=(5, 1), font=font, pad=(2, 2)))
+            allocation_layout.append(row)
