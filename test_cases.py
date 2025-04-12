b7645e4bea5c6fd6b3acef42cbc90d33623c9776
mansirathor27 <mansirathor575@gmail.com>
2025-04-12 20:03:23 +0530

    Added sample data demonstrating a safe state

--- test_cases.py
+++ test_cases.py
@@ -24,3 +24,27 @@ def get_deadlock_case():
     available = np.array([0, 0, 0])
     
     return allocation, max_matrix, available
+def get_no_deadlock_case():
+    """Return sample data that demonstrates a safe state (no deadlock)."""
+    
+    allocation = np.array([
+        [0, 1, 0],
+        [2, 0, 0],
+        [3, 0, 2],
+        [2, 1, 1],
+        [0, 0, 2]
+    ])
+    
+   
+    max_matrix = np.array([
+        [7, 5, 3],
+        [3, 2, 2],
+        [9, 0, 2],
+        [2, 2, 2],
+        [4, 3, 3]
+    ])
+    
+    
+    available = np.array([10, 5, 7])
+    
+    return allocation, max_matrix, available
